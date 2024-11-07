a07da4af8f3f091490a2c0ca5d46528abfdb73ff
SeventhDream <68449189+SeventhDream@users.noreply.github.com>
2024-11-07 16:23:15 +1300

    Implemented random object motion in 'RandomPatrol.cs' script
    - Added variables to define screen boundary coordinates.
    - Implemented GetRandomPosition() function to generate random position coordinates within screen boundaries.
    - Added if else statement in the Update() function to object towards target position at a defined speed and generate a new target position by calling GetRandomPosition() when the previous target has already been reached.

--- Assets/RandomPatrol.cs
+++ Assets/RandomPatrol.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class RandomPatrol : MonoBehaviour
 {
+    // Define position values for the edge of the screen (based on circle size).
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    Vector2 targetPosition; // Stores target (x,y) coordinates.
+
+    public float speed; // Defines how fast the object moves to target position.
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,23 @@ public class RandomPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move towards target position if not there already.
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        // Generate new random target position if previous target has already been reached.
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    // Generate random position coordinates within screen boundaries.
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
     }
 }
